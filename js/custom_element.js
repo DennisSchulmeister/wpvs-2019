28f324ff93e119c6d977f4012fb951d4b12cf99d
Dennis Schulmeister-Zimolong <dennis@patk.org>
2019-07-13 15:45:40 +0200

    New API and responsiveness for <wpvs-tab-pages>
    This commit greatly improves the <wpvs-tab-pages> element by making it
    responsive and scriptable. It now contains several data-attributes like
    data-active-tab, data-mode and data-breakpoint to define its behaviour.

--- js/custom_element.js
+++ js/custom_element.js
@@ -22,14 +22,14 @@ class CustomElement extends HTMLElement {
      * Constructor as required for custom elements. This makes sure that the
      * element will only be rendered once the document has finished loading.
      * Because otherwise rendering will most likely start too soon. It also
-     * registers a MutationObserver to rerender the element if its contant
+     * registers a MutationObserver to rerender the element if its content
      * has changed.
      */
     constructor() {
         super();
 
-        this.mutationObserver = new MutationObserver(() => this._rerenderContent);
-        this.mutationObserver.observe(this, {attributes: true, childList: true, subtree: true});
+        this._childMutationObserver = null;
+        this._attributeMutationObserver = null;
 
         if (document.readyState === "complete") {
             this.render();
@@ -39,10 +39,75 @@ class CustomElement extends HTMLElement {
     }
 
     /**
-     * Internal callback to completely (re)render the custom element.
-     * Needs to be overwritten by the sub-classes.
+     * Internal method to call the subclasses `render()` method. It makes sure
+     * to disable all MutationObservers before calling `render()` and enabling
+     * them again afterwards, because otherwise we would have an infinite loop.
+     *
+     * If a custom element wants to rerender its content it also must call this
+     * method instead of render() to prevent an infinite loop.
      */
     render() {
+        this._disableObservers();
+        this._render();
+        this._enableObservers();
+    }
+
+    /**
+     * Internal callback to completely render the custom element. Needs to be
+     * overwritten by the sub-classes. Never call it manually!
+     */
+    _render() {
+    }
+
+    /**
+    * Internal method to call the subclasses `_onAttributeChanged()` method.
+    * It makes sure to disable all MutationObservers before calling
+    * `_onAttributeChanged()` and enabling them again afterwards, because
+    * otherwise we could produce an infinite loop.
+     */
+    onAttributeChanged(mutations) {
+        this._disableObservers();
+        this._onAttributeChanged(mutations);
+        this._enableObservers();
+    }
+
+    /**
+     * Internal callback to be overwritten by subclasses. This allows a custom
+     * element to detect, when one of its HTML attributes has changed. The
+     * element might update its DOM or call the render() method if needed.
+     *
+     * @param {MutationRecord[]} mutations Array of all detected changes,
+     *   see: https://developer.mozilla.org/en-US/docs/Web/API/MutationRecord
+     */
+    _onAttributeChanged(mutations) {
+    }
+
+    /**
+     * Disable all mutation observers to prevent an infinite loop while modifying
+     * the DOM tree. This doesn't need to be called inside the inherited `_render()`
+     * method. But it can be called on other places by the subclasses to prevent
+     * infinite loops.
+     */
+    _disableObservers() {
+        if (this._childMutationObserver) this._childMutationObserver.disconnect();
+        if (this._attributeMutationObserver) this._attributeMutationObserver.disconnect();
+    }
+
+    /**
+     * Reenable all mutation observers again. This doesn't need to be called inside
+     * the inherited `_render()` method. But it can be called on other places by
+     * the subclasses where they also called `_disableObservers()` to reenable
+     * them again.
+     */
+    _enableObservers() {
+        this._disableObservers();
+
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/MutationObserverInit
+        this._childMutationObserver = new MutationObserver((mutations, observer) => this.render());
+        this._childMutationObserver.observe(this, {childList: true, subtree: true, characterData: true});
+
+        this._attributeMutationObserver = new MutationObserver((mutations, observer) => this.onAttributeChanged(mutations));
+        this._attributeMutationObserver.observe(this, {attributes: true, attributeOldValue: true, subtree: false});
     }
 
     /**
