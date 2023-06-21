de57989067e4f7b4a439b11fb4ee56c6f93f70fb
Dennis Schulmeister-Zimolong <dennis@patk.org>
2023-06-21 19:46:32 +0200

    Docstring for scripts/build-static.js explaingin the new small macro language

--- scripts/build-static.mjs
+++ scripts/build-static.mjs
@@ -1,3 +1,44 @@
+/**
+ * Utility script to copy static assets from the source directory into the build directory.
+ * Includes some special logic for text files in which simple macros can be extended.
+ * For this the following settings can be made in file `package.json`:
+ * 
+ * ````js
+ * {
+ *     "config": {
+
+ *       "static_dir": "static",                           // Source directory from which to copy files
+ *       "build_dir": "build",                             // Target directory to which to copy files
+ *       "replace_variables_extensions": ".htm; .html"     // Extensions of text files with macros
+ *     },
+ * }
+ * ```
+ * 
+ * Files without one of the named extensions will simply be copied. Text files, however,
+ * will be read line by line and rewritten, expanding macros found in those files along
+ * the way (1-pass). Macros can be defined like this:
+ * 
+ * ```
+ * {define macro-name}
+ *     Content of the macro, using variables: {variable1}, {variable2}
+ * {enddefine}
+ * ```
+ * 
+ * The `{define}` and `{enddefine}` statements must be standing in a separate line each,
+ * ignoring leading and trailing whitespace. The macros can then be inserted at other
+ * positions like this, again with `{insert}` and `{endinsert}` in separate lines:
+ * 
+ * ```
+ * {insert macro-name}
+ *     variable1 = value 1
+ *     variable2 = value 2
+ * {endinsert}
+ * ```
+ * 
+ * If the name of a variable starts with `url-` or ends with `-url` its value will be
+ * URL encoded. Otherwise it will be replaced verbatim, only removing leading and trailing
+ * whitespace.
+ */
 "use strict";
 
 import fs       from "node:fs";
